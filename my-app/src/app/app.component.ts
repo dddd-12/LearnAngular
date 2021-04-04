2a308ddb8f44203c91a228355af8bfc179afbc35
dddd-12 <tan893525@163.com>
2021-04-05 01:16:50 +0800

    2021.4.4 angular学习

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -14,14 +14,12 @@ import { Component } from '@angular/core';
 //元数据（metadata）--最基础最开始的，告诉angular如何处理一个类用装饰器（decorator）来附加元数据给一个类（class），ng将其视为一个组件
 @Component({ //--decorator
   selector: 'app-root', //当前组件的引用地址-将组件渲染到app-root中   
-  // templateUrl: './app.component.html', //组件模板
-  template: `
-    <h1>hello ES6 的模板字符串---{{title}}</h1><hr>
-    <hello-world></hello-world><hr>
-    <hello-everyone></hello-everyone>
-  `,
-  // styleUrls: ['./app.component.css'] //组件的样式文件
-  styles: [`h1{color:#369;}`]
+  templateUrl: './app.component.html', //组件模板
+  // template: `
+  //   <h1>hello ES6 的模板字符串---{{title}}</h1><hr>
+  // `,
+  styleUrls: ['./app.component.css'] //组件的样式文件
+  // styles: [`h1{color:#369;}`]
 })
 export class AppComponent { //--class组件类，导出：组件名称
   title = 'My first angular project: my-app';
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,22 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; //引用小组件，在declarations中声明
 // 引入组件
 import { HelloWorldComponent } from './hello-world.component';
 import { HelloEveryoneComponent } from './hello-everyone/hello-everyone.component';
+import { HelloCssComponent } from './hello-css/hello-css.component';
+import { HelloBindComponent } from './hello-bind/hello-bind.component';
+import { HelloSelfEventComponent } from './hello-self-event/hello-self-event.component';
+import { HelloSelfChild } from './hello-self-event/hello-self-child.component';
+import { HelloTwoWayComponent } from './hello-two-way/hello-two-way.component';
+import { HelloInputOutputComponent } from './hello-input-output/hello-input-output.component';
+import { HelloInputChild } from './hello-input-output/hello-input.component';
+import { HelloTemplateComponent } from './hello-template/hello-template.component';
+import { HelloSelfDirectiveComponent } from './hello-self-directive/hello-self-directive.component';
+import { AppHighlightDirective } from "./highlight.directive.component"
 
 @NgModule({ //装饰器函数：用于接收一个描述模块属性的元数据对象
   // 使用组件时必须先声明组件
   declarations: [//声明本模块中拥有的视图类--组件、指令、管道
     AppComponent,
     HelloWorldComponent,
-    HelloEveryoneComponent
+    HelloEveryoneComponent,
+    HelloCssComponent,
+    HelloBindComponent,
+    HelloSelfEventComponent,
+    HelloSelfChild,
+    HelloTwoWayComponent,
+    HelloInputOutputComponent,
+    HelloInputChild,
+    HelloTemplateComponent,
+    HelloSelfDirectiveComponent,
+    AppHighlightDirective
   ],
   imports: [ //引入本模块需要的类，兼容性组件-其他模块
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [], //服务的创建者，并加入到全局服务列表中，可用于应用部分
   bootstrap: [AppComponent] //指定应用的主视图（称为根组件），它是所有其他视图的宿主，只有根模块才可以设置bootstrap属性（引导组件-AppComponent）
