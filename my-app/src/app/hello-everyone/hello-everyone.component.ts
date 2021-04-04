6ecc6f9788c977e78c0d49a0d4effca4dcea5d66
dddd-12 <tan893525@163.com>
2021-04-05 01:13:59 +0800

    结构指令的使用-*ngIf/*ngFor/ngSwitch等

--- my-app/src/app/hello-everyone/hello-everyone.component.ts
+++ my-app/src/app/hello-everyone/hello-everyone.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hello-everyone.component.css']
 })
 export class HelloEveryoneComponent implements OnInit {
-
+  flag=true;
+  citys = ['shanghai','beijing','hangzhou'];//在组件中定义一个数组
+  fruits = [
+    {id:1, name:'apple'},
+    {id:2, name:'banner'},
+    {id:3, name:'orange'}
+  ];
+  char = 'A';
   constructor() { }
 
   ngOnInit(): void {
