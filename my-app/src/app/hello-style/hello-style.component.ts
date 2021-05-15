b866b03c22a31cb5f9df475c4d5c83470ef66c11
dddd-12 <tan893525@163.com>
2021-05-15 16:22:53 +0800

    组件样式的使用方式

---
+++ my-app/src/app/hello-style/hello-style.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-style',
+  templateUrl: './hello-style.component.html',
+  styleUrls: ['./hello-style.component.css'],
+  // styles:[
+  //   `p{font-size:24px;}`,
+  //   `*{color:red}`
+  // ]
+})
+export class HelloStyleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
